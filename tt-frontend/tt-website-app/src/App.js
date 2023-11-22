342e78e73b63c98db96e661a79bb8b524955abf8
BarrosoGit <barroso.eduardo001@gmail.com>
2023-11-22 14:29:06 -0600

    working version of the app

--- tt-frontend/tt-website-app/src/App.js
+++ tt-frontend/tt-website-app/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Amplify from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
